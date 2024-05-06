d81dc697c6604c5eb08c0ce5742a45b9a4863912
Mirxonjon <mirxonjonismanov152@gmail.com>
2024-05-07 02:57:55 +0500

    fixed bug

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
+import { userModule } from './module/user/user.module';
+import { ResumeModule } from './module/resume/resume.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { config } from './config';
 import { connectDb } from './config/typeorm';
 import { AuthModule } from './module/auth/auth.module';
-import {  ResumeModule } from './module/application/resume.module';
-import {  userModule } from './module/mainServiseCategory/user.module';
 import { JobModule } from './module/job/job.module';
 import { LikeModule } from './module/like/like.module';
 import { ResponseModule } from './module/response/response.module';
